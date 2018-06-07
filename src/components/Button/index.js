16b36cf287034488644e9563ca4598dcc874775a
Tamara Jordan <tamarajordan14@gmail.com>
2018-06-07 10:50:46 -0700

    move ButtonLoader to its own component rather than sticking it inside of Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,86 +1,7 @@
 import React from 'react'
 import cn from 'classnames'
-
 import { string, func, bool, node, oneOfType, arrayOf } from 'prop-types'
-
-const getLoader = () => (
-  <div className='loader'>
-    <svg
-      width='120'
-      height='30'
-      viewBox='0 0 120 30'
-      xmlns='http://www.w3.org/2000/svg'
-      fill='#fff'
-    >
-      <circle cx='15' cy='15' r='15'>
-        <animate
-          attributeName='r'
-          from='15'
-          to='15'
-          begin='0s'
-          dur='0.8s'
-          values='15;9;15'
-          calcMode='linear'
-          repeatCount='indefinite'
-        />
-        <animate
-          attributeName='fill-opacity'
-          from='1'
-          to='1'
-          begin='0s'
-          dur='0.8s'
-          values='1;.5;1'
-          calcMode='linear'
-          repeatCount='indefinite'
-        />
-      </circle>
-      <circle cx='60' cy='15' r='9' fillOpacity='0.3'>
-        <animate
-          attributeName='r'
-          from='9'
-          to='9'
-          begin='0s'
-          dur='0.8s'
-          values='9;15;9'
-          calcMode='linear'
-          repeatCount='indefinite'
-        />
-        <animate
-          attributeName='fill-opacity'
-          from='0.5'
-          to='0.5'
-          begin='0s'
-          dur='0.8s'
-          values='.5;1;.5'
-          calcMode='linear'
-          repeatCount='indefinite'
-        />
-      </circle>
-      <circle cx='105' cy='15' r='15'>
-        <animate
-          attributeName='r'
-          from='15'
-          to='15'
-          begin='0s'
-          dur='0.8s'
-          values='15;9;15'
-          calcMode='linear'
-          repeatCount='indefinite'
-        />
-        <animate
-          attributeName='fill-opacity'
-          from='1'
-          to='1'
-          begin='0s'
-          dur='0.8s'
-          values='1;.5;1'
-          calcMode='linear'
-          repeatCount='indefinite'
-        />
-      </circle>
-    </svg>
-  </div>
-)
+import ButtonLoader from '../ButtonLoader'
 
 const Button = ({
   children,
@@ -109,8 +30,6 @@ const Button = ({
     },
   )
 
-  const loader = loading && getLoader()
-
   return (
     <button
       className={classNames}
@@ -120,7 +39,7 @@ const Button = ({
       <span>
         {children}
       </span>
-      {loader}
+      {loading && <ButtonLoader />}
     </button>
   )
 }
